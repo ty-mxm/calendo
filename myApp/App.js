e34d9d0f6612871ecb93c1af36d7936dacad35ca
ty-mxm <165325425+ty-mxm@users.noreply.github.com>
2024-12-11 17:43:33 -0500

    Ajout du home screen

--- myApp/App.js
+++ myApp/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/Home/HomeScreen';
+import GetStartedScreen from './src/screens/Home/GetStartedScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="GetStarted">
+        <Stack.Screen
+          name="GetStarted"
+          component={GetStartedScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Home' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ myApp/src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      {/* Icône principale */}
+  
+
+      {/* Boutons */}
+      <TouchableOpacity style={styles.createButton}>
+        <Text style={styles.createButtonText}>Create a team</Text>
+      </TouchableOpacity>
+      <Text style={styles.orText}>or</Text>
+      <TouchableOpacity style={styles.joinButton}>
+        <Text style={styles.joinButtonText}>Join a team</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F4F4F4',
+  },
+  icon: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  createButton: {
+    backgroundColor: '#7F57FF',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+    marginBottom: 10,
+  },
+  createButtonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  orText: {
+    fontSize: 16,
+    color: '#000',
+    marginVertical: 10,
+  },
+  joinButton: {
+    backgroundColor: '#34C759',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 30,
+  },
+  joinButtonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
